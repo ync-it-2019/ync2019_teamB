252c8294d8e3f2a5936cadfda23a2349b02adead
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-10-21 05:38:21 +0000

    #이주현

---
+++ branches/teamB_project/src/main/java/com/ync/project/domain/MeetingVO.java
@@ -0,0 +1,15 @@
+package com.ync.project.domain;
+
+public class MeetingVO {
+	
+	private int meeting_Num;			//모임 번호
+	
+	private String meeting_Name;		//모임 이름
+	private String introduce;			//모임 소개
+	private String userid;				//모임장 아이디
+	private String meeting_Profile;		//모임 이미지
+	
+	private String meeting_Adress;		//모임 활동지역
+	private String meeting_Hobby;		//모임 관심사
+	
+}
