a953d5f2ed7638dcb0a0d79c68bc8630652c53b8
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-10-28 00:20:38 +0000

    #이주현

--- branches/teamB_project/src/main/java/com/ync/project/domain/Meeting_MemberVO.java
+++ branches/teamB_project/src/main/java/com/ync/project/domain/Meeting_MemberVO.java
@@ -2,6 +2,12 @@ package com.ync.project.domain;
 
 import java.util.Date;
 
+ /**
+  * @FileName	: Meeting_MemberVO.java
+  * @Date		: 2019. 10. 25. 
+  * @Author		: 이주현
+  * @프로그램 설명 : 모임 회원VO
+  */
 public class Meeting_MemberVO {
 	
 	private int meeting_Num;		//모임 번호
