a690db38e370a78d1e6a57b50eb89a611c374026
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-10-28 00:20:01 +0000

    #이주현

--- branches/teamB_project/src/main/java/com/ync/project/domain/AppointmentVO.java
+++ branches/teamB_project/src/main/java/com/ync/project/domain/AppointmentVO.java
@@ -2,6 +2,12 @@ package com.ync.project.domain;
 
 import java.util.Date;
 
+ /**
+  * @FileName	: AppointmentVO.java
+  * @Date		: 2019. 10. 25. 
+  * @Author		: 이주현
+  * @프로그램 설명 : 정모 VO
+  */
 public class AppointmentVO {
 
 	private int appointment_Num;		//정모 번호
