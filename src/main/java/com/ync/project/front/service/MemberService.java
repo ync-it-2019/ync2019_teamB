b44fd3786b2f22c122038327f1c5f2770d712326
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-11-03 10:07:35 +0000

    #조중현 맴버 서비스

---
+++ src/main/java/com/ync/project/front/service/MemberService.java
@@ -0,0 +1,26 @@
+package com.ync.project.front.service;
+
+import java.util.List;
+
+import com.ync.project.domain.MemberVO;
+
+ /**
+  * @FileName	: MemberService.java
+  * @Date		: 2019. 10. 30. 
+  * @Author		: 조중현
+  * @프로그램 설명 : 회원 서비스
+  */
+public interface MemberService {
+	
+	public void register(MemberVO member);
+	
+	public MemberVO info(String userid);
+	
+	public boolean modify(MemberVO member);
+
+	public List<MemberVO> getUserList();
+//	
+
+//	
+//	public int updateUser(MemberVO MemberVO);
+}
