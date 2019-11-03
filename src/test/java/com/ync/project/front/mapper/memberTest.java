8315868a8e2475a7de2074424ab8e94fedc526c8
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-11-03 10:08:49 +0000

    #조중현 맴버 mapper test

---
+++ src/test/java/com/ync/project/front/mapper/memberTest.java
@@ -0,0 +1,67 @@
+package com.ync.project.front.mapper;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ync.project.domain.MemberVO;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+
+@Log4j
+public class memberTest {
+
+	@Autowired
+	
+	private MemberMapper mapper;
+	
+	@Test
+	public void testGetList() {
+		mapper.getUserList().forEach(member -> log.info(member));
+	}
+	
+//	@Test
+//	public void testRege() {
+//		
+//		MemberVO member = new MemberVO();
+//		member.setName("김상훈");
+//		member.setUserid("user01");
+//		member.setUserpw("1234");
+//		member.setPhone("010-4944-6748");
+//		member.setEmail("user01@ync.ac.kr");
+//		member.setAdress("대구광역시");
+//		member.setHobby("메이플");
+//		
+//		mapper.insert(member);
+//		log.info(member);
+//		
+//	}
+	
+//	@Test
+//	public void testRead() {
+//		MemberVO member = mapper.read("user01");
+//		
+//		log.info(member);
+//	}
+	
+//	@Test
+//	public void testUpdate() {
+//		MemberVO member = new MemberVO();
+//		
+//		member.setUserid("user01");
+//		member.setUserpw("12345");
+//		member.setPhone("010-1111-2222");
+//		member.setAdress("대구");
+//		member.setHobby("슈우미");
+//		member.setProfile("사아아진");
+//		
+//		mapper.updateUser(member);
+//	}
+}
