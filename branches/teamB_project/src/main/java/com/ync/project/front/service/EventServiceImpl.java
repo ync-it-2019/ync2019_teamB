45ddfbadb80ef5fb80f762e36f6e4ffd28b1f467
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-11-05 03:33:06 +0000

    #김상훈 이벤트, 공지사항, 고객센터 서비스

---
+++ branches/teamB_project/src/main/java/com/ync/project/front/service/EventServiceImpl.java
@@ -0,0 +1,51 @@
+package com.ync.project.front.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ync.project.domain.Criteria;
+import com.ync.project.domain.EventVO;
+import com.ync.project.front.mapper.EventMapper;
+
+import lombok.extern.log4j.Log4j;
+
+ /**
+  * @FileName	: EventServiceImpl.java
+  * @Date		: 2019. 10. 27. 
+  * @Author		: 김상훈
+  * @프로그램 설명 : 이벤트 ServiceImpl
+  */
+@Log4j
+@Service
+public class EventServiceImpl implements EventService {
+
+	@Autowired
+	private EventMapper mapper;
+
+	@Override
+	public EventVO read(Long event_num) {
+
+		log.info("get......" + event_num);
+
+		return mapper.read(event_num);
+
+	}
+
+	 @Override
+	 public List<EventVO> getList() {
+	
+		 log.info("getList..........");
+		
+		 return mapper.getList();
+	 }
+
+	@Override
+	public List<EventVO> getListWithPaging(Criteria cri) {
+
+		log.info("get List with criteria: " + cri);
+
+		return mapper.getListWithPaging(cri);
+	}
+}
---
+++ branches/teamB_project/src/main/java/com/ync/project/front/service/QuestionService.java
@@ -0,0 +1,31 @@
+package com.ync.project.front.service;
+
+import java.util.List;
+
+import com.ync.project.domain.Criteria;
+import com.ync.project.domain.QuestionVO;
+
+
+ /**
+  * @FileName	: QuestionService.java
+  * @Date		: 2019. 10. 27. 
+  * @Author		: 김상훈
+  * @프로그램 설명 : 고객센터 Service
+  */
+public interface QuestionService {
+	
+	// 글 등록
+	public void register(QuestionVO board);
+		
+	// 글 상세보기
+	public QuestionVO read(Long question_num);
+	
+	// 글 수정
+	public boolean modify(QuestionVO board);
+	
+	// 전체 글 목록
+	public List<QuestionVO> getList();
+	
+	// 글 목록 페이징 
+	public List<QuestionVO> getListWithPaging(Criteria cri);
+}
