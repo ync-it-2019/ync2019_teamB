3c5c42905a6a44c874c711136a4505617e012eb0
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-11-05 03:18:36 +0000

    #김상훈 이벤트 ,공지사항, 고객센터 서비스

---
+++ branches/teamB_project/src/test/java/com/ync/project/service/NoticeServiceTests.java
@@ -0,0 +1,43 @@
+package com.ync.project.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ync.project.domain.Criteria;
+import com.ync.project.front.service.NoticeService;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class NoticeServiceTests {
+
+	@Autowired
+	private NoticeService service;
+
+	@Test
+	public void testExist() {
+
+		log.info(service);
+		assertNotNull(service);
+	}
+	
+	@Test
+	public void testGetList() {
+
+		service.getListWithPaging(new Criteria(1, 10)).forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testGet() {
+
+		log.info(service.read(1L));
+	}
+
+}
