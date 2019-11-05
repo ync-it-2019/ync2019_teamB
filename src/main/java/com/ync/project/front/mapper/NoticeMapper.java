45939149204b3338bc414c3292c3004d7922f814
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-11-05 04:49:59 +0000

    #김상훈 공지사항 Mapper

---
+++ src/main/java/com/ync/project/front/mapper/NoticeMapper.java
@@ -0,0 +1,23 @@
+package com.ync.project.front.mapper;
+
+import java.util.List;
+
+import com.ync.project.domain.Criteria;
+import com.ync.project.domain.NoticeVO;
+
+ /**
+  * @FileName	: noticeMapper.java
+  * @Date		: 2019. 10. 23. 
+  * @Author		: 김상훈
+  * @프로그램 설명 : 공지사항 Mapper interface
+  */
+public interface NoticeMapper {
+	// 전체 글 목록
+	public List<NoticeVO> getList();
+	// 글 상세보기
+	public NoticeVO read(Long notice_num);
+	// 검색
+	public void insert(NoticeVO board);
+	// 글 목록 페이징 
+	public List<NoticeVO> getListWithPaging(Criteria cri);
+}
