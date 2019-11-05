ee194f27cbbf6fd53672f003a5913928c1075b6c
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-11-05 04:47:46 +0000

    #김상훈 이벤트 맵퍼

--- src/main/java/com/ync/project/front/mapper/EventMapper.java
+++ src/main/java/com/ync/project/front/mapper/EventMapper.java
@@ -9,15 +9,13 @@ import com.ync.project.domain.EventVO;
   * @FileName	: EventMapper.java
   * @Date		: 2019. 10. 21. 
   * @Author		: 김상훈
-  * @프로그램 설명 : 이벤트 mapper
+  * @프로그램 설명 : 이벤트 Mapper interface
   */
 public interface EventMapper {
-	//전체 글 목록
+	// 전체 글 목록
 	public List<EventVO> getList();
 	// 글 상세보기
-	public EventVO get(Long event_num);
-	// 글 목록 페이징 
+	public EventVO read(Long event_num);
+	// 목록 페이징
 	public List<EventVO> getListWithPaging(Criteria cri);
-	
-	public int getTotalCount(Criteria cri);
 }
