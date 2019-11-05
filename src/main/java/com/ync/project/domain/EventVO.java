07ed3681f48bb9fe4afb109acbeecaa7d5bf18f5
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-11-05 05:01:04 +0000

    #김상훈 이벤트 VO

--- src/main/java/com/ync/project/domain/EventVO.java
+++ src/main/java/com/ync/project/domain/EventVO.java
@@ -13,12 +13,12 @@ import lombok.Data;
   */
 @Data
 public class EventVO {
-	  private Long event_no;		//글번호
+	  private Long event_num;		//글번호
 	  private String title;			//제목
 	  private Date event_start_date;//이벤트 시작일
 	  private Date event_end_date;  //이벤트 종료일
 	  private String contents;		//내용
-	  private Date write_Date;		//작성일자
+	  private Date write_date;		//작성일자
 	  private String image;			//사진
 	  private String userid;		//글쓴이 아이디
 	  private String banner_image;  //배너 이미지
