188c7232a171ab143b5ce4e9b1cb9b9a32354b5d
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-11-11 07:07:17 +0000

    #김상훈 이벤트 컨트롤러

--- src/main/java/com/ync/project/front/controller/EventController.java
+++ src/main/java/com/ync/project/front/controller/EventController.java
@@ -1,36 +1,44 @@
 package com.ync.project.front.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ync.project.front.service.EventService;
+
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
-	 /**
-	  * @FileName	: EventController.java
-	  * @Date		: 2019. 10. 20. 
-	  * @Author		: 김상훈
-	  * @프로그램 설명 : 이벤트 호출용 컨트롤러
-	  */
-	@Controller
-	@Log4j
-	@RequestMapping("/front/event/")
+ /**
+  * @FileName	: EventController.java
+  * @Date		: 2019. 11. 5. 
+  * @Author		: 김상훈
+  * @프로그램 설명 : 이벤트 호출용 컨트롤러
+  */
+@Controller
+@Log4j
+@RequestMapping("/front/*")
+@AllArgsConstructor
 public class EventController {
-		@Autowired
-		@GetMapping(value = "/list")
-		public void evnetList() {
-			log.info("eventList!");
-		}
-		
-		@GetMapping(value = "/endList")
-		public void eventEndList() {
-			log.info("eventList!");
-		}
-		
-		@GetMapping(value = "/get")
-		public void evnetGet() {
-			log.info("notice get!");
-
-		}
+   private EventService service;
+   
+   @GetMapping("/event/list")
+   public void list(Model model) {
+      log.info("list");
+      model.addAttribute("eventList", service.getList());
+   }
+   
+   @GetMapping("/event/endList")
+   public void endList(Model model) {
+      log.info("endList");
+      model.addAttribute("eventEndList", service.getList());
+   }
+   
+   @GetMapping("/event/get")
+   public void get(@RequestParam("event_num") Long event_num, Model model) {
+      log.info("get");
+      model.addAttribute("board", service.read(event_num));
+   }
 }
