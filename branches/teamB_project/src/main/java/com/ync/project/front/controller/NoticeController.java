c5418910701396b6523c299a04dce3f53b10b6bc
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-11-11 07:07:37 +0000

    #김상훈 공지사항 컨트롤러

--- branches/teamB_project/src/main/java/com/ync/project/front/controller/NoticeController.java
+++ branches/teamB_project/src/main/java/com/ync/project/front/controller/NoticeController.java
@@ -1,33 +1,32 @@
 package com.ync.project.front.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ync.project.front.service.NoticeService;
+
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
-	 /**
-	  * @FileName	: NoticeController.java
-	  * @Date		: 2019. 10. 20. 
-	  * @Author		: 김상훈
-	  * @프로그램 설명 : 공지사항 호출용 컨트롤러
-	  */
-	@Controller
-	@Log4j
-	@RequestMapping("/front/notice/")
+@Controller
+@Log4j
+@RequestMapping("/front/*")
+@AllArgsConstructor
 public class NoticeController {
-
-		@Autowired
-		@GetMapping(value = "/list")
-		public void NoticeList() {
-			log.info("noticeList!");
-		}
-		
-		@GetMapping(value = "/get")
-		public void NoticeGet() {
-			log.info("noticeGet!");
-
-		}
-		
+   private NoticeService service;
+   
+   @GetMapping("/notice/list")
+   public void list(Model model) {
+      log.info("list");
+      model.addAttribute("noticeList", service.getList());
+   }
+   
+   @GetMapping("/notice/get")
+   public void get(@RequestParam("notice_num") Long notice_num, Model model) {
+      log.info("get");
+      model.addAttribute("board", service.read(notice_num));
+   }
 }
