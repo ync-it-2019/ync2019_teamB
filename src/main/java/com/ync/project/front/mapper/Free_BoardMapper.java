ea6d529ce47b3653e3238db8a689a4af40142197
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-11-27 08:13:25 +0000

    /**  * @FileName	: MeetingController.java  * @Date		: 2019. 11. 27.  * @Author		: 장윤석  * @프로그램 설명     : free_board_mapper  */

--- src/main/java/com/ync/project/front/mapper/Free_BoardMapper.java
+++ src/main/java/com/ync/project/front/mapper/Free_BoardMapper.java
@@ -2,14 +2,19 @@ package com.ync.project.front.mapper;
 
 import java.util.List;
 
-//import org.apache.ibatis.annotations.Param;
 import com.ync.project.domain.Criteria;
 import com.ync.project.domain.Free_BoardVO;
 
+/**
+ * @FileName	: MeetingController.java
+ * @Date		: 2019. 11. 27. 
+ * @Author		: 장윤석
+ * @프로그램 설명     : free_board_mapper
+ */
+
 public interface Free_BoardMapper {
 	
-	// @Select("SELECT * FROM tbl_board WHERE bno > 0")
-	public List<Free_BoardVO> getList();
+	public List<Free_BoardVO> getList(); //글목록
 
 	public List<Free_BoardVO> getListWithPaging(Criteria cri); //글 목록 페이징
 
@@ -17,13 +22,13 @@ public interface Free_BoardMapper {
 
 	public Integer insertSelectKey(Free_BoardVO board);
 
-	public Free_BoardVO read(Long free_board_num);
+	public Free_BoardVO read(Long free_board_num);  //글 상세보기   모임 번호 필요?
 
 	public int delete(Long free_board_num);
 
 	public int update(Free_BoardVO board);
 
-	public int getTotalCount(Criteria cri);  //글 목록 갯수 카운트?
+	public int getTotalCount(Criteria cri);  //글 목록 갯수 카운트
 
 	// 2개 이상의 파라미터를 넘기기위해 @Param 사용. 댓글 추가/삭제 시 amount에 1/-1 값
 //	public void updateReplyCnt(@Param("free_board_num") Long bno, @Param("amount") int amount); //리플 갯수 새는 테이블이 없음
