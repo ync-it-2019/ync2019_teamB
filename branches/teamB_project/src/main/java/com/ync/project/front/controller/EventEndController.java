129cacc5b832bac0275020bbddc772d97fac72bb
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-12-02 12:17:30 +0000

    #김상훈 종료된 이벤트 리스트 호출용 컨트롤러

---
+++ branches/teamB_project/src/main/java/com/ync/project/front/controller/EventEndController.java
@@ -0,0 +1,32 @@
+package com.ync.project.front.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ync.project.front.service.EventEndService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+ /**
+  * @FileName	: EventEndController.java
+  * @Date		: 2019. 11. 27. 
+  * @Author		: 김상훈
+  * @프로그램 설명 : 종료된 이벤트 호출용 컨트롤러
+  */
+@Controller
+@Log4j
+@RequestMapping("/front/*")
+@AllArgsConstructor
+public class EventEndController {
+   private EventEndService service;
+   //종료된 이벤트 목록
+   @GetMapping("/event/endList")
+   public void endList(Model model) {
+      log.info("endList");
+      model.addAttribute("eventEndList", service.getList());
+   }
+}
