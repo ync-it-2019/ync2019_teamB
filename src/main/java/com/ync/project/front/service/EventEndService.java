7bcd6219dc492546171b5a4c06999cabd58361d6
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-12-02 12:18:35 +0000

    #김상훈 종료된 이벤트 서비스

---
+++ src/main/java/com/ync/project/front/service/EventEndService.java
@@ -0,0 +1,23 @@
+package com.ync.project.front.service;
+
+import java.util.List;
+
+import com.ync.project.domain.Criteria;
+import com.ync.project.domain.EventVO;
+
+ /**
+  * @FileName	: EventEndService.java
+  * @Date		: 2019. 11. 27. 
+  * @Author		: 김상훈
+  * @프로그램 설명 : 종료된 이벤트 Service
+  */
+public interface EventEndService {
+	// 글 상세보기
+	public EventVO read(Long event_num);
+	
+	// 전체 글 목록
+	public List<EventVO> getList();
+	
+	// 글 목록 페이징 
+	public List<EventVO> getListWithPaging(Criteria cri);
+}
