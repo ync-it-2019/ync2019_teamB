79988abd4142986077ad2a70c1ec3b73b6f17438
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-12-04 07:49:01 +0000

    /**  * @FileName	: Free_BoardMapperTest.java  * @Date		: 2019. 12. 04.  * @Author		: 장윤석  * @프로그램 설명     : 모임 게시판 매퍼 테스트  */

--- branches/teamB_project/src/test/java/com/ync/project/front/mapper/Free_BoardMapperTests.java
+++ branches/teamB_project/src/test/java/com/ync/project/front/mapper/Free_BoardMapperTests.java
@@ -7,8 +7,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import com.ync.project.domain.Free_BoardVO;
+
 import com.ync.project.domain.Criteria;
+import com.ync.project.domain.Free_BoardVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -25,24 +26,26 @@ public class Free_BoardMapperTests {
 //	public void testGetList() {
 //		
 //		mapper.getList().forEach((board) -> {
+//			board.setMeeting_num(2L);
 //			log.info("==================");
 //			log.info(board);
 //			});
 //
 //	}
+	
 
 //	@Test
 //	public void testInsert() {   
 //
 //		Free_BoardVO board = new Free_BoardVO();
-//		board.setMeeting_num(1);
-//		board.setFree_board_num(81);
+//		board.setMeeting_num(2L);
+//		board.setFree_board_num(201L);
 //		board.setUserid("user01");
-//		board.setTitle("새로 작성하는 글 select key");
+//		board.setTitle("테스트 중입니다.");
 //		board.setCategory("자유");
-//		board.setContents("새로 작성하는 내용 select key");
+//		board.setContents("다른 모임 게시물");
 //		board.setFiles("ABC");
-//		board.setViews(0);	
+//		board.setViews(0L);	
 //		mapper.insert(board);
 //		log.info(board);
 //	}
@@ -51,14 +54,14 @@ public class Free_BoardMapperTests {
 //	   public void testInsertSelectKey() {
 //
 //	      Free_BoardVO board = new Free_BoardVO();
-//	      board.setFree_board_num(95);
-//	      board.setMeeting_num(1);
+//	      board.setFree_board_num(154L);
+//	      board.setMeeting_num(2L);
 //	      board.setUserid("user01");
 //	      board.setTitle("수정된 제목");
 //	      board.setContents("수정된 내용");
 //	      board.setFiles("파일A");
 //	      board.setCategory("자유");
-//	      board.setViews(1);
+//	      board.setViews(0L);
 //	      
 //	      mapper.insertSelectKey(board);
 //	      log.info(board);
@@ -101,25 +104,25 @@ public class Free_BoardMapperTests {
 //		Criteria cri = new Criteria();
 //		
 //	    //10개씩 3페이지 
-//	    cri.setPageNum(3);
-//	    cri.setAmount(20);
-//
-//		List<Free_BoardVO> list = mapper.getListWithPaging(cri);
+//	    cri.setPageNum(2);
+//	    cri.setAmount(10);
+//	    
+//		List<Free_BoardVO> list = mapper.getListWithPaging(cri, 1L);
 //		list.forEach(board -> log.info(board));
 //
 //	}
 
-//	  @Test 
-//	  public void testSearch() {  
-//
-//	    Criteria cri = new Criteria();
-//	    cri.setKeyword("키워드");
-//	    cri.setType("TC");
-//
-//	    List<Free_BoardVO> list = mapper.getListWithPaging(cri);
-//
-//	    list.forEach(board -> log.info(board));
-//	  }
+	  @Test 
+	  public void testSearch() {  
+
+	    Criteria cri = new Criteria();
+	    cri.setKeyword("키워드");
+	    cri.setType("TC");
+
+	    List<Free_BoardVO> list = mapper.getListWithPaging(cri,1L);
+
+	    list.forEach(board -> log.info(board));
+	  }
 	  
 //	  @Test
 //	  public void testTotal() {
