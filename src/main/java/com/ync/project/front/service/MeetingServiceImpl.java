2d101840f267f774a332492095e200def8c2d5fd
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-12-12 13:54:52 +0000

    #조중현 MeetingVO Mapper, Service

--- src/main/java/com/ync/project/front/service/MeetingServiceImpl.java
+++ src/main/java/com/ync/project/front/service/MeetingServiceImpl.java
@@ -14,73 +14,82 @@ import lombok.extern.log4j.Log4j;
 
 
  /**
-  * @FileName	: MeetingServiceImpl.java
-  * @Date		: 2019. 11. 27. 
-  * @Author		: 조중현
+  * @FileName   : MeetingServiceImpl.java
+  * @Date      : 2019. 11. 27. 
+  * @Author      : 조중현
   * @프로그램 설명 : 모임 서비스 implements
   */
 @Log4j
 @Service
 public class MeetingServiceImpl implements MeetingService {
 
-	@Autowired
-	private MeetingMapper mapper;
+   @Autowired
+   private MeetingMapper mapper;
 
-	@Override
-	public MeetingVO read(Long meeting_num) {
+   @Override
+   public MeetingVO read(Long meeting_num) {
 
-		log.info("get......" + meeting_num);
-		
+      log.info("get......" + meeting_num);
+      
 
-		return mapper.read(meeting_num);
+      return mapper.read(meeting_num);
 
-	}
+   }
 
-	 @Override
-	 public List<MeetingVO> getList() {
-	
-		 log.info("getList..........");
-		
-		 return mapper.getList();
-	 }
+    @Override
+    public List<MeetingVO> getList() {
+   
+       log.info("getList..........");
+      
+       return mapper.getList();
+    }
 
-	@Override
-	public List<MeetingVO> getListWithPaging(Criteria cri) {
+   @Override
+   public List<MeetingVO> getListWithPaging(Criteria cri) {
 
-		log.info("get List with criteria: " + cri);
+      log.info("get List with criteria: " + cri);
 
-		return mapper.getListWithPaging(cri);
-	}
+      return mapper.getListWithPaging(cri);
+   }
 
-	@Override
-	public List<MeetingVO> getMyList(Criteria cri, String userid) {
-		
-		log.info("get my List: " + cri);
-		return mapper.getMyList(cri, userid);
-	}
+   @Override
+   public List<MeetingVO> getMyList(Criteria cri, String userid) {
+      
+      log.info("get my List: " + cri);
+      return mapper.getMyList(cri, userid);
+   }
 
-	@Override
-	public int getTotal(Criteria cri) {
-		log.info("get total count");
-		return mapper.getTotalCount(cri);
-	}
-	
-	@Override
-	public void insertMember(Meeting_MemberVO mMember) {
-		log.info("insert Member");
-		mapper.insertMember(mMember);
-	}
-	
-	@Override
-	public int getMyTotal(String userid) {
-		log.info("get My Meeting List total count");
-		return mapper.getMyTotalCount(userid);
-	}
+   @Override
+   public int getTotal(Criteria cri) {
+      log.info("get total count");
+      return mapper.getTotalCount(cri);
+   }
 
-	@Override
-	public List<MeetingVO> MeetingMemberCount() {
-		log.info("get My Meeting List Member total count");
-		return mapper.MeetingMemberCount();
-	}
-	
+   @Override
+   public int getMyTotal(String userid) {
+      log.info("get My Meeting List total count");
+      return mapper.getMyTotalCount(userid);
+   }
+
+   @Override
+   public List<MeetingVO> MeetingMemberCount() {
+      log.info("get My Meeting List Member total count");
+      return mapper.MeetingMemberCount();
+   }
+
+   
+   @Override
+   public void insertMember(Meeting_MemberVO mMember) {
+      log.info("insert Member");
+      mapper.insertMember(mMember);
+   }
+
+   @Override
+   public List<MeetingVO> mainMeetingList() {
+      log.info("get mainMeetingList!");
+      return mapper.mainMeetingList();
+   }
+   
+
+   
 }
