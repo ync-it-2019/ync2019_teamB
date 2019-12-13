5e2011cf66480af839707a8411416ad36cea2053
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-12-13 06:19:20 +0000

    #서영준

--- branches/teamB_project/src/main/java/com/ync/project/admin/controller/AdminMemberController.java
+++ branches/teamB_project/src/main/java/com/ync/project/admin/controller/AdminMemberController.java
@@ -36,7 +36,6 @@ public class AdminMemberController {
 	  * @return call jsp view
 	  */
 	@GetMapping("/list") 
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public void list(Criteria cri, Model model) {
 	  
 		log.info("Member List get page!");
