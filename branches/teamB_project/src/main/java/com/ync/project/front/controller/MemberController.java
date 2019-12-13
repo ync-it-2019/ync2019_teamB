0126f9a39dbbf14640d158d11fd29ab6623fa6a1
team_b <team_b@e39eca35-2b93-4401-a5e7-378c213998bc>
2019-12-13 11:46:59 +0000

    #조중현 회원가입페이지 (아이디 중복확인 구현)

--- branches/teamB_project/src/main/java/com/ync/project/front/controller/MemberController.java
+++ branches/teamB_project/src/main/java/com/ync/project/front/controller/MemberController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,7 +42,9 @@ public class MemberController {
 	  * @작성자 : 조중현
 	  */
 	@GetMapping("/register")
-	public void register() {
+	public void register(Model model) {
+		log.info("Register page!");
+		model.addAttribute("member", service.getUserList());
 	}
 	
 	 /**
